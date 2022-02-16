b55c651fd2ebffa69594a610d7d9622fbc96dd54
Kartheek Janapati <kartheekj59@gmail.com>
2022-02-15 23:21:06 -0500

    #6-fix hostname and port error for flask app

--- data-retrival-service/main.py
+++ data-retrival-service/main.py
@@ -47,6 +47,6 @@ def get_image():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8000)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
