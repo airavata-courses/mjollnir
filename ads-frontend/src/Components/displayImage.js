56cc5d47fa88d47f0896e46531cd0cb3fa642bd1
srnida <srnida@iu.edu>
2022-05-04 02:39:54 -0400

    shell code

--- ads-frontend/src/Components/displayImage.js
+++ ads-frontend/src/Components/displayImage.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
-import {sendData,getRadarStations} from '../api_calls.js';
+// import {sendData,getRadarStations} from '../api_calls.js';
 
 export default (props) =>(
     <div>
         {props.image ? <center><img src={props.image} ></img></center> : <div/>}
     </div>
 );
-
 
 
 
